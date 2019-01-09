79df9aafdc647fc84429c40b3381b16effb29994
lutiantian <cztian1314@163.com>
2019-01-09 17:29:55 +0800

    2019.1.9

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// class App extends Component {
+//     render() {
+//         return ( <
+//             div className = "App" >
+//             <
+//             header className = "App-header" >
+//             <
+//             img src = { logo }
+//             className = "App-logo"
+//             alt = "logo" / >
+//             <
+//             p >
+//             Edit < code > src / App.js < /code> and save to reload. </p > < a className = "App-link"
+//             href = "https://reactjs.org"
+//             target = "_blank"
+//             rel = "noopener noreferrer" >
+//             Learn React < /a> </header >
+//             <
+//             /div>
+//         );
+//     }
+// }
+
+// export default App;
+import React, { Component } from 'react'
+
+class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { date: new Date() }
+    }
+    componentDidMount() {
+        this.timeID = setInterval(() => this.tick(), 1000)
+    }
+    componentWillUnmount() {
+        clearInterval()
+    }
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+    render() {
+        return ( <
+            div >
+            <
+            h1 > hello world < /h1> 
+
+            <
+            h2 > This is { this.state.date.toLocaleTimeString() } < /h2> < /
+            div >
+        )
+    }
 }
-
-export default App;
+export default Clock;
